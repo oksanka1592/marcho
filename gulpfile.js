78e56ba4bd1b31fc0125ff2b7f2add48a10a4db2
oksanka1592 <ksuxa15lz@gmail.com>
2022-12-20 14:31:28 +0300

    gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,7 @@ function browsersync() {
 
 function styles() {
   return src('app/scss/style.scss')
-    .pipe(scss({ outputStyle: 'compressed' }))
+    .pipe(scss({ outputStyle: 'expanded' }))
     .pipe(concat('style.min.css'))
     .pipe(autoprefixer({
       overrideBrowserslist: ['last 10 version'],
